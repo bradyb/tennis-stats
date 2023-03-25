2bc864584ac03473e2d4777e2d398d52d5c2d048
Raghav Prabhu <rprab@umich.edu>
2023-03-25 17:55:24 -0400

    Fix French Open to not use Selenium.

--- french_open_stats.py
+++ french_open_stats.py
@@ -1,12 +1,13 @@
-import json
+import csv
 import re
-import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+
+def split_objects(s):
+    objects = []
+    reader = csv.reader([s], delimiter=',', quotechar='"', escapechar='\\')
+    for row in reader:
+        objects.append(row)
+    return objects
 
 # TODO: Is it possible to calculate playerId from playerName?
 class FrenchOpenUrlConstructor:
@@ -44,75 +45,152 @@ class FrenchOpenStatsCalculator:
     """
     Class for calculating fantasy tennis stats for the Australian Open.
     """
-    def __init__(self, round, playerId, gender):
+    def __init__(self, round, playerId, gender, http):
         self.team = self.getTeam(round, playerId)
         self.displayUrl = FrenchOpenUrlConstructor(playerId=playerId,gender=gender,round=round).getUrl()
-        self.soup = self.getSoup(playerId, gender, round)
+        self.soup = self.getMatchStats(playerId, gender, round, http)
         self.team = self.getTeam(round, playerId)
     
     def getDisplayUrl(self):
         return self.displayUrl
     
-    def getSoup(self, playerId, gender, round):
+    def getMatchStats(self, playerId, gender, round, http):
         matchUrl = FrenchOpenUrlConstructor(playerId=playerId,gender=gender,round=round).getUrl()
-        options = Options()
-        options.add_argument("--window-size=1920,1200")
-        options.add_argument('--headless=new')
-        driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
-        driver.get(matchUrl)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        driver.quit()
+        resp = http.request("GET", matchUrl)
+        if (resp.status != 200):
+            print("Error fetching URL:", matchUrl)
 
-        match_stats = soup.find("div", id="RGMatchStats")
-        soup = BeautifulSoup(str(match_stats), 'html.parser')
+        html = resp.data.decode('utf-8')
+        soup = BeautifulSoup(html, "html.parser")
+        scripts = soup.find_all('script')
 
-        return soup
+        # Get the script containing the match stats.
+        statsScript = None
+        for script in scripts:
+            if (len(script.contents) > 0 and "window.__NUXT__" in script.contents[0]):
+                statsScript = script.contents[0]
+        
+        self.variableMap = self.getJsVariableMap(statsScript)
+        self.matchStats = self.getMatchStatisticsSection(statsScript)
+        
+    def getMatchStatisticsSection(self, statsScript):
+        searchTerm = "matchStatistics:{"
+        startIdx = statsScript.find(searchTerm)
+
+        i = startIdx + len(searchTerm) - 1
+        bracesCount = 0
+        while (i < len(statsScript)):
+            if statsScript[i] == "{":
+                bracesCount = bracesCount + 1
+            if statsScript[i] == "}":
+                bracesCount = bracesCount - 1
+            i = i + 1
+            if bracesCount == 0:
+                break
+
+        return statsScript[startIdx:i]
+
+    def getJsVariableMap(self, statsScript):
+        searchTerm = "window.__NUXT__=(function"
+
+        # Get a list of the function arguments.
+        startIdx = statsScript.find(searchTerm)
+        i = startIdx + len(searchTerm)
+        while (i < len(statsScript) and statsScript[i] != ")"):
+            i = i + 1
+        varNames = statsScript[startIdx + len(searchTerm) + 1:i]
+        varNameList = varNames.split(",")
+
+        # Get a list of the argument values.
+        startIdx = statsScript.find("}(")
+        endIdx = statsScript.find("));")
+        varValues = statsScript[startIdx + 2:endIdx]
+        varValueList = split_objects(varValues)[0]
+
+        assert(len(varNameList) == len(varValueList))
+
+        # Construct map from variable name to value.
+        returnMap = {}
+        for i in range(len(varNameList)):
+            returnMap[varNameList[i]] = varValueList[i]
+        return returnMap
 
     def getTeam(self, round, playerId):
         while (round > 1):
             modulus = playerId % 2
             playerId = playerId // 2 + modulus
             round = round - 1
-        return ('player1' if (playerId % 2) == 1 else 'player2')
+        return ('teamAValue' if (playerId % 2) == 1 else 'teamBValue')
 
-    def getSingleTextStatByName(self, name):
-        statSections = self.soup.find_all(string=re.compile(name))
-        statSection = statSections[0].parent.parent.parent.parent
-       
-        if self.team == "player1":
-            statSelf = statSection.find("div", {"class": re.compile(r'.*p1Stats.*')})
-            return statSelf.text
+    def getNumericalStatByName(self, name):
+        searchTerm = name + ":{"
+    
+        statIdx = self.matchStats.find(searchTerm)
+        statSection = self.matchStats[statIdx:]
 
-        if self.team == "player2":
-            statSelf = statSection.find("div", {"class": re.compile(r'.*p2Stats.*')})
-            return statSelf.text
+        valueIdx = statSection.find(self.team)
+        valueSection = statSection[valueIdx:]
+        i = 0
+        while (i < len(valueSection)):
+            char = valueSection[i]
+            if char in "},":
+                break
+            i = i + 1
+        stat = valueSection[len(self.team)+1:i]
+        try:
+            int(stat.strip('"'))
+            return int(stat.strip('"'))
+        except:
+            return int(self.variableMap[stat])
 
-    def getFractionStatByName(self, name):
-        fraction_regex = r'\d+/\d+ '
-        statSections = self.soup.find_all(string=re.compile(name))
-        statSection = statSections[0].parent.parent.parent.parent
-       
-        if self.team == "player1":
-            statSelf = statSection.find("div", {"class": re.compile(r'.*p1Stats.*')})
-            statList = statSelf.find_all("div", {"class": re.compile(r'.*player1 non-speed')})
-            for stat in statList:
-                if (re.fullmatch(fraction_regex, stat.text)):
-                    numerator, denominator = int(stat.text.split("/")[0]), int(stat.text.split("/")[1])
-                    return int(round(100 * numerator/denominator))
 
-        if self.team == "player2":
-            statSelf = statSection.find("div", {"class": re.compile(r'.*p2Stats.*')})
-            statList = statSelf.find_all("div", {"class": re.compile(r'.*player2 non-speed')})
-            for stat in statList:
-                if (re.fullmatch(fraction_regex, stat.text)):
-                    numerator, denominator = int(stat.text.split("/")[0]), int(stat.text.split("/")[1])
-                    return int(round(100 * numerator/denominator))
+        return stat.strip('"')
+
+    def getFractionalStatByName(self, name, getOpponentStat=False):
+        team = self.team
+        if getOpponentStat:
+            if team == "teamAValue":
+                team = "teamBValue"
+            elif team == "teamBValue":
+                team = "teamAValue"
+        
+        searchTerm = name + ":{"
+    
+        statIdx = self.matchStats.find(searchTerm)
+        statSection = self.matchStats[statIdx:]
+
+        valueIdx = statSection.find(team)
+        valueSection = statSection[valueIdx:]
+        i = 0
+        while (i < len(valueSection)):
+            char = valueSection[i]
+            if char in "},":
+                break
+            i = i + 1
+        stat = valueSection[len(team)+1:i]
+
+        # Define the regular expression pattern to match the integers
+        pattern = r'\d+'
+
+        # Find all occurrences of the pattern in the string
+        matches = re.findall(pattern, stat.strip('"'))
+        integers = [int(match) for match in matches]
+
+        if (len(integers) == 4):
+            return integers[0], integers[2]
+        else:
+            stat = self.variableMap[stat]
+            matches = re.findall(pattern, stat.strip('"'))
+            integers = [int(match) for match in matches]
+
+            if (len(integers) == 4):
+                return integers[0], integers[2]
 
     def getAces(self):
-        return self.getSingleTextStatByName("Aces")
+        return self.getNumericalStatByName("aces")
     
     def getDoubleFaults(self):
-        return self.getSingleTextStatByName("Double faults")
+        return self.getNumericalStatByName("doubleFaults")
 
     def getServe(self):
         numAces = self.getAces()
@@ -122,10 +200,10 @@ class FrenchOpenStatsCalculator:
         return 3 * (int(numAces) - int(numDoubleFaults))
 
     def getWinners(self):
-        return self.getSingleTextStatByName("Winners")
+        return self.getNumericalStatByName("winners")
 
     def getUnforcedErrors(self):
-        return self.getSingleTextStatByName("Unforced errors")
+        return self.getNumericalStatByName("unforcedErrors")
 
     def getPower(self):
         numWinners = int(self.getWinners())
@@ -133,52 +211,20 @@ class FrenchOpenStatsCalculator:
         return 2 * numWinners - numUnforcedErrors
 
     def getReturns(self):
-        return self.getFractionStatByName("Receiving points won")
+        returnWon, returnPlayed = self.getFractionalStatByName("returnPoints")
+        return round(100 * float(returnWon) / float(returnPlayed))
 
     def getDefense(self):
-        return self.getFractionStatByName("Win on 2nd serve")
+        pointsWon, pointsPlayed = self.getFractionalStatByName("winRateOnSecondServe")
+        return round(100 * float(pointsWon) / float(pointsPlayed))
 
     def getMind(self):
-        statSections = self.soup.find_all(string=re.compile("Break points won"))
-        statSection = statSections[0].parent.parent.parent.parent
-        bpWon, bpSaved, numBroken = 0, 0, 0
-        fraction_regex = r'\d+/\d+ '
-       
-        if self.team == "player1":
-            # Get own player's break point stats.
-            bpSection = statSection.find("div", {"class": re.compile(r'.*p1Stats.*')})
-            bpSection = bpSection.find_all("div", {"class": re.compile(r'.*player1 non-speed')})
-            for section in bpSection:
-                if (re.fullmatch(fraction_regex, section.text)):
-                    bpWon = int(section.text.split("/")[0])
-                    break
-            # Get opponent's break point stats.
-            bpSection = statSection.find("div", {"class": re.compile(r'.*p2Stats.*')})
-            bpSection = bpSection.find_all("div", {"class": re.compile(r'.*player2 non-speed')})
-            for section in bpSection:
-                if (re.fullmatch(fraction_regex, section.text)):
-                    bpSaved = int(section.text.split("/")[1]) - int(section.text.split("/")[0])
-                    numBroken = int(section.text.split("/")[0])
-                    break
+        bpWon, _ = self.getFractionalStatByName("breakPoint")
+        numTimesBroken, bpFaced = self.getFractionalStatByName("breakPoint", getOpponentStat=True)
 
-        if self.team == "player2":
-            # Get own player's break point stats.
-            bpSection = statSection.find("div", {"class": re.compile(r'.*p2Stats.*')})
-            bpSection = bpSection.find_all("div", {"class": re.compile(r'.*player2 non-speed')})
-            for section in bpSection:
-                if (re.fullmatch(fraction_regex, section.text)):
-                    bpWon = int(section.text.split("/")[0])
-                    break
-            # Get opponent's break point stats.
-            bpSection = statSection.find("div", {"class": re.compile(r'.*p1Stats.*')})
-            bpSection = bpSection.find_all("div", {"class": re.compile(r'.*player1 non-speed')})
-            for section in bpSection:
-                 if (re.fullmatch(fraction_regex, section.text)):
-                    bpSaved = int(section.text.split("/")[1]) - int(section.text.split("/")[0])
-                    numBroken = int(section.text.split("/")[0])
-                    break
+        bpSaved = bpFaced - numTimesBroken
         
-        return 5 * bpSaved + 5 * bpWon - 3 * numBroken
+        return 5 * bpWon + 5 * bpSaved - 3 * numTimesBroken
     
     def getStat(self, stat):
         if (stat == "Serve"):
--- pull_tennis_stats.py
+++ pull_tennis_stats.py
@@ -14,22 +14,11 @@ http = urllib3.PoolManager(
 # round is the round of the tournament (1-7)
 # playerId is the position in the draw (for example, the 2022 Men's draw goes Djokovic=1, Nishioka=2, Molcan=3, Coria=4, ...)
 # gender is M or F
-fs = FrenchOpenStatsCalculator(round=1, playerId=124, gender="F")
-print("Stats URL:", fs.getDisplayUrl())
-print("Aces:", fs.getAces())
-print("Double faults:", fs.getDoubleFaults())
-print("Serve:", fs.getServe())
-print("Winners:", fs.getWinners())
-print("Unforced errors:", fs.getUnforcedErrors())
-print("Power:", fs.getPower())
-print("Return:", fs.getReturns())
-print("Defense:", fs.getDefense())
-print("Mind:", fs.getMind())
 
-# with open('input.csv', newline='') as csvfile:
-#     fileReader = csv.reader(csvfile, delimiter=',', quotechar='|')
-#     for row in fileReader:
-#         playerName, gender, playerId, stat, round = row[0], row[1], int(row[2]), row[3], int(row[4])
-#         ts = AusOpenStatsCalculator(round, playerId, gender, http)
-#         print("Stat:", stat,  "for player:", playerName, "in round:", round, "is:", ts.getStat(stat))
-#         print("Match stats URL is:", ts.getDisplayUrl(), "\n")
+with open('input.csv', newline='') as csvfile:
+    fileReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in fileReader:
+        playerName, gender, playerId, stat, round = row[0], row[1], int(row[2]), row[3], int(row[4])
+        ts = FrenchOpenStatsCalculator(round, playerId, gender, http)
+        print("Stat:", stat,  "for player:", playerName, "in round:", round, "is:", ts.getStat(stat))
+        print("Match stats URL is:", ts.getDisplayUrl(), "\n")
